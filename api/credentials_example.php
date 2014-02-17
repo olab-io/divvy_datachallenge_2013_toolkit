8677a8eccda85be2104b4aec79ffe77e72073004
Christopher Baker <me@christopherbaker.net>
2014-02-17 16:39:35 -0500

    Update to new credential system.

---
+++ api/credentials_example.php
@@ -0,0 +1,15 @@
+<?php
+
+// The name of the host.  Usually localhost.
+$host   = "localhost";	
+
+// The name of the database.  The default is divvy_2013.
+$dbname = "divvy_2013";
+
+// The mysql databse username used to access the database.
+$user   = "MYSQL_USERNAME";
+
+// The mysql database password associated with the above username.
+$pass   = "MYSQL_PASSWORD";
+
+?>  
