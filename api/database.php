6facb48ad1dc7009d77298a4bf34df26d930cf9a
Christopher Baker <me@christopherbaker.net>
2014-02-17 12:54:42 -0500

    Move database to the api.

--- api/database.php
+++ api/database.php
@@ -1,8 +1,9 @@
 <?php
  // Connect to the divvy database.
- mysql_connect("DATABASE_HOST",
-               "DATABASE_USERNAME",    
-               "DATABAES_PASSWORD") or die(mysql_error());
+ mysql_connect("HOSTNAME",
+               "USERNAME",    
+               "PASSWORD") or die(mysql_error());
  // Select the divvy database. 
  mysql_select_db("divvy_2013") or die(mysql_error());   
 ?>  
+
